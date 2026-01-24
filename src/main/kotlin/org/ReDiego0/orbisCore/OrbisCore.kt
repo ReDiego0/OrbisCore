b6f992b5c6ece81eadb9550d8d4665a2480df236
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-24 01:08:00 -0300

    chore: Initial commit

---
+++ src/main/kotlin/org/ReDiego0/orbisCore/OrbisCore.kt
@@ -0,0 +1,14 @@
+package org.ReDiego0.orbisCore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OrbisCore : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
