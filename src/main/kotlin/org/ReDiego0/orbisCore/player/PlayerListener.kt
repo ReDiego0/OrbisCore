6568f93c4e27e6d134e8c2c72485b0cf3cf496a6
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-24 02:24:43 -0300

    refactor: Remove unused PlayerManager import from PlayerListener

--- src/main/kotlin/org/ReDiego0/orbisCore/player/PlayerListener.kt
+++ src/main/kotlin/org/ReDiego0/orbisCore/player/PlayerListener.kt
@@ -1,6 +1,5 @@
 package org.ReDiego0.orbisCore.player
 
-import org.ReDiego0.orbisCore.modules.player.PlayerManager
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent
