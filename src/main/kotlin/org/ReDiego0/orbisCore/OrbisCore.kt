3c7367ece5a6588c5ef9dd9f90fa679a8e0fdf45
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-24 01:24:58 -0300

    feat: Integrate PlayerManager and PlayerListener into OrbisCore initialization flow

--- src/main/kotlin/org/ReDiego0/orbisCore/OrbisCore.kt
+++ src/main/kotlin/org/ReDiego0/orbisCore/OrbisCore.kt
@@ -1,14 +1,26 @@
 package org.ReDiego0.orbisCore
 
+import org.ReDiego0.orbisCore.modules.player.PlayerListener
+import org.ReDiego0.orbisCore.modules.player.PlayerManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class OrbisCore : JavaPlugin() {
 
+    lateinit var playerManager: PlayerManager
+        private set
+
     override fun onEnable() {
-        // Plugin startup logic
+        logger.info("Iniciando OrbisCore...")
+
+        playerManager = PlayerManager(this)
+        server.pluginManager.registerEvents(PlayerListener(playerManager), this)
+
+        logger.info("OrbisCore cargado correctamente.")
     }
 
     override fun onDisable() {
-        // Plugin shutdown logic
+        if (::playerManager.isInitialized) {
+            playerManager.saveAll()
+        }
     }
 }
