90c0d8362992af317cba63209fc922b859548a60
ReDiego0 <re.diego.maldonado@gmail.com>
2026-01-27 22:37:30 -0300

    refactor: Replace SkillExecutor and CooldownManager with SkillManager; simplify combat logic and enhance skill handling

--- src/main/kotlin/org/ReDiego0/orbisCore/OrbisCore.kt
+++ src/main/kotlin/org/ReDiego0/orbisCore/OrbisCore.kt
@@ -1,7 +1,6 @@
 package org.ReDiego0.orbisCore
 
 import org.ReDiego0.orbisCore.combat.CombatListener
-import org.ReDiego0.orbisCore.combat.SkillExecutor
 import org.ReDiego0.orbisCore.commands.OrbisCommand
 import org.ReDiego0.orbisCore.config.ClassRegistry
 import org.ReDiego0.orbisCore.ether.EtherTask
@@ -14,6 +13,9 @@ import org.ReDiego0.orbisCore.player.OrbisPapiExpansion
 import org.ReDiego0.orbisCore.player.PlayerListener
 import org.ReDiego0.orbisCore.player.PlayerManager
 import org.ReDiego0.orbisCore.skills.SkillManager
+import org.ReDiego0.orbisCore.skills.implemented.FlechaPerforante
+import org.ReDiego0.orbisCore.skills.implemented.GolpeSismico
+import org.ReDiego0.orbisCore.skills.implemented.RayoArcano
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -22,8 +24,6 @@ class OrbisCore : JavaPlugin() {
     lateinit var playerManager: PlayerManager
         private set
     lateinit var classRegistry: ClassRegistry
-        private set
-    lateinit var skillExecutor: SkillExecutor
         private set
     lateinit var itemProvider: ItemProvider
         private set
@@ -44,6 +44,18 @@ class OrbisCore : JavaPlugin() {
 
         skillManager = SkillManager(this)
 
+        // Registro de skills
+        // Vanguardia
+        skillManager.registerSkill(GolpeSismico(this))
+
+        // Tejedor
+        skillManager.registerSkill((RayoArcano(this)))
+
+        // Cazador
+        skillManager.registerSkill(FlechaPerforante(this))
+
+        skillManager.reloadSkillsConfig()
+
         itemProvider = ItemProvider(this)
 
         instanceRegistry = InstanceRegistry(this)
@@ -52,8 +64,7 @@ class OrbisCore : JavaPlugin() {
         playerManager = PlayerManager(this)
         server.pluginManager.registerEvents(PlayerListener(playerManager), this)
 
-        skillExecutor = SkillExecutor(this)
-        server.pluginManager.registerEvents(CombatListener(this, skillExecutor), this)
+        server.pluginManager.registerEvents(CombatListener(this), this)
         server.pluginManager.registerEvents(MobLootListener(this), this)
 
         getCommand("orbis")?.setExecutor(OrbisCommand(this))
--- src/main/kotlin/org/ReDiego0/orbisCore/combat/CombatListener.kt
+++ src/main/kotlin/org/ReDiego0/orbisCore/combat/CombatListener.kt
@@ -1,40 +1,42 @@
 package org.ReDiego0.orbisCore.combat
 
+import net.kyori.adventure.text.Component
 import org.ReDiego0.orbisCore.OrbisCore
 import org.ReDiego0.orbisCore.config.SkillSlot
+import org.bukkit.Material
+import org.bukkit.Sound
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerSwapHandItemsEvent
 
-class CombatListener(private val plugin: OrbisCore, private val executor: SkillExecutor) : Listener {
-
+class CombatListener(private val plugin: OrbisCore) : Listener {
     @EventHandler
     fun onDrop(event: PlayerDropItemEvent) {
         val player = event.player
         val data = plugin.playerManager.getPlayerData(player.uniqueId) ?: return
+        if (data.equippedSkills[SkillSlot.Q] == null) return
 
-        val skillId = data.equippedSkills[SkillSlot.Q]
+        event.isCancelled = true
 
-        if (skillId != null) {
-            // Cancelamos el drop para que no tire su espada al suelo
-            event.isCancelled = true
-            executor.tryCast(player, skillId)
-        }
+        plugin.skillManager.castSlot(player, SkillSlot.Q)
     }
 
     @EventHandler
     fun onSwapHand(event: PlayerSwapHandItemsEvent) {
         val player = event.player
         val data = plugin.playerManager.getPlayerData(player.uniqueId) ?: return
+        if (data.equippedSkills[SkillSlot.F] == null) return
 
-        val skillId = data.equippedSkills[SkillSlot.F]
+        event.isCancelled = true
 
-        if (skillId != null) {
-            // Cancelamos el cambio de mano
-            event.isCancelled = true
-
-            executor.tryCast(player, skillId)
+        val item = player.inventory.itemInMainHand
+        if (item.type == Material.AIR) {
+            player.sendActionBar(Component.text("§c¡Necesitas un arma para canalizar Éter!"))
+            player.playSound(player.location, Sound.BLOCK_CHAIN_HIT, 1f, 2f)
+            return
         }
+
+        plugin.skillManager.castSlot(player, SkillSlot.F)
     }
 }
